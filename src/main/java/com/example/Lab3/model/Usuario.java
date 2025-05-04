81678ed19ec575abea106a6bbe2671d2150cbec8
MTaveiras <marcosctaveira@gmail.com>
2025-05-04 18:39:40 -0300

    Criando a primeira classe

---
+++ src/main/java/com/example/Lab3/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.example.Lab3.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
+public abstract class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    public abstract String logar(String email, String senha);
+}
