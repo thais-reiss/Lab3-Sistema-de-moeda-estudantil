ff6b15b04d90936405d074d8a79521f48f14d5a1
MTaveiras <marcosctaveira@gmail.com>
2025-06-02 10:03:03 -0300

    Pequenos ajustes

--- code/src/main/java/com/example/Lab3/Lab3Application.java
+++ code/src/main/java/com/example/Lab3/Lab3Application.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/*
+   cd code
+ * mvn clean package
+   mvn spring-boot:run
+ */
+
 @SpringBootApplication
 public class Lab3Application {
 
